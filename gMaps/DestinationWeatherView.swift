53035f2f5436ef0ab7be4fa37b9c8091cdee28d1
Jose Mejia <josemejia@joses-macbook-air.local>
2017-04-30 00:14:34 -0500

    -Add Destination weather details

---
+++ gMaps/DestinationWeatherView.swift
@@ -0,0 +1,108 @@
+//
+//  DestinationWeatherView.swift
+//  gMaps
+//
+//  Created by Jose Mejia on 4/29/17.
+//  Copyright © 2017 ITink. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class DestinationWeatherView: UIView {
+    
+    @IBInspectable var tempText = "Temperature: ---ºC" {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var humText = "Humidity: ---%" {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var pressText = "Pressure: ----hPa" {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var locationName = "Medellín, Antioquia" {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    let temperatureLabel = UILabel()
+    let humidityLabel = UILabel()
+    let pressureLabel = UILabel()
+    let locationLabel = UILabel()
+    let closeButton = UIButton()
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        setup()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    func setup() {
+        
+        //locationLabel.backgroundColor = UIColor.red
+        locationLabel.textColor = UIColor.white
+        locationLabel.font = UIFont(name: "HelveticaNeue-Regular", size: 19)
+        locationLabel.text = String(locationName)
+        locationLabel.textAlignment = NSTextAlignment.justified
+        
+        //temperatureLabel.backgroundColor = UIColor.red
+        temperatureLabel.textColor = UIColor.white
+        temperatureLabel.font = UIFont(name: "HelveticaNeue-Regular", size: 19)
+        temperatureLabel.text = String(tempText)
+        temperatureLabel.textAlignment = NSTextAlignment.justified
+        
+        //humidityLabel.backgroundColor = UIColor.red
+        humidityLabel.textColor = UIColor.white
+        humidityLabel.font = UIFont(name: "HelveticaNeue-Regular", size: 19)
+        humidityLabel.text = String(humText)
+        humidityLabel.textAlignment = NSTextAlignment.justified
+        
+        //pressureLabel.backgroundColor = UIColor.red
+        pressureLabel.textColor = UIColor.white
+        pressureLabel.font = UIFont(name: "HelveticaNeue-Regular", size: 19)
+        pressureLabel.text = String(pressText)
+        pressureLabel.textAlignment = NSTextAlignment.justified
+        
+        //closeButton.backgroundColor = UIColor.red
+        closeButton.setTitle("X", for: .normal)
+        closeButton.setTitleColor(UIColor.red, for: UIControlState.normal)
+        closeButton.titleLabel?.font = UIFont(name: "HelveticaNeue-Bold", size: 23)
+        
+        addSubview(locationLabel)
+        addSubview(temperatureLabel)
+        addSubview(humidityLabel)
+        addSubview(pressureLabel)
+        addSubview(closeButton)
+
+    }
+    
+    override func layoutSubviews() {
+        locationLabel.frame = CGRect(x: 2, y: 0, width: bounds.size.width/2, height: bounds.size.height/4)
+        temperatureLabel.frame = CGRect(x: 2, y: bounds.size.height/4, width: bounds.size.width/2, height: bounds.size.height/4)
+        humidityLabel.frame = CGRect(x: 2, y: (bounds.size.height/4)*2, width: bounds.size.width/2, height: bounds.size.height/4)
+        pressureLabel.frame = CGRect(x: 2, y: (bounds.size.height/4)*3, width: (bounds.size.width/2), height: bounds.size.height/4)
+        closeButton.frame = CGRect(x: (bounds.size.width-20), y: 2, width: 20, height: 20)
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- gMaps/ViewController.swift
+++ gMaps/ViewController.swift
@@ -13,14 +13,13 @@ import GooglePlaces
 class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDelegate, GMSAutocompleteViewControllerDelegate {
     
     @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var destinationWeatherView: DestinationWeatherView!
     @IBOutlet weak var temperature: tempView!
     var popOverVC = PopupViewController()
     
     let locationManager = CLLocationManager()
     let myLocationMarker = GMSMarker()
     
-    @IBOutlet weak var PressureLabel: UILabel!
-    @IBOutlet weak var humidityLabel: UILabel!
     var pressureText:String!
     var humidityText:String!
     var weatherText:String!
@@ -28,6 +27,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDel
     
     var imgIconUrl:URL!
     var isStationAvailable = true
+    
+    let RESIZE_FACTOR = CGFloat(80.0)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,9 +40,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDel
         temperature.detailPopup.addTarget(self, action: #selector(ViewController.popUpView), for: UIControlEvents.touchUpInside)
         
         temperature.refreshButton.addTarget(self, action: #selector(ViewController.updateWeather), for: UIControlEvents.touchUpInside)
+        destinationWeatherView.closeButton.addTarget(self, action: #selector(ViewController.closeWeatherDetails), for: UIControlEvents.touchUpInside)
         
         self.temperature.detailPopup.isHidden = true
         self.temperature.refreshButton.isHidden = true
+        self.destinationWeatherView.isHidden = true
         
         
     }
@@ -82,8 +85,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDel
             imgName="rainy"
         }
         
-        
-        
         self.popOverVC.weatherImgIcon.image = UIImage(named:imgName)
         
     }
@@ -107,10 +108,19 @@ class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDel
         marker.title = "\(place.name)"
         marker.map = mapView
         
+        howsTheWeatherThere(placeName: place.name, coordinate: place.coordinate)
+        
+        self.mapView.frame = CGRect(x: 0, y: 0, width: self.mapView.frame.width, height: (self.mapView.frame.height-RESIZE_FACTOR))
+        
         self.dismiss(animated: true, completion: nil) // dismiss after select place
         
     }
     
+    func howsTheWeatherThere(placeName:String, coordinate:CLLocationCoordinate2D ) {
+        self.destinationWeatherView.isHidden = false
+        self.destinationWeatherView.locationLabel.text = placeName
+        destinationRequest (coordinates:coordinate)
+    }
     
     func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
         
@@ -295,6 +305,68 @@ class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDel
                 
                 self.pressureText = "\(doubleStr) Atm"
                 self.humidityText = "\(NSNumber(value: (results["main"]?["humidity"]) as! Double))%"
+            })
+            
+            }.resume()
+        
+    }
+    
+    func closeWeatherDetails(){
+        self.destinationWeatherView.isHidden = true
+        self.mapView.frame = CGRect(x: 0, y: 0, width: self.mapView.frame.width, height: (self.mapView.frame.height+(RESIZE_FACTOR+20)))
+        self.mapView.clear()
+        mapView.camera = GMSCameraPosition.camera(withLatitude: (locationManager.location?.coordinate.latitude)!, longitude: (locationManager.location?.coordinate.longitude)!, zoom: 17)
+    }
+    
+    func destinationRequest (coordinates:CLLocationCoordinate2D) {
+        
+        let urlString = "http://api.openweathermap.org/data/2.5/weather?lat=\(coordinates.latitude)&lon=\(coordinates.longitude)&APPID=722e31fed4758ec43905b234ed67369d&lang=sp"
+        
+        var request = URLRequest(url: URL(string: urlString)!)
+        
+        request.httpMethod = "GET"
+        let session = URLSession.shared
+        
+        session.dataTask(with: request) {data, response, error in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+            guard let data = data else {
+                print("Data is empty")
+                return
+            }
+            
+            let json = try! JSONSerialization.jsonObject(with: data, options: []) as! Dictionary<String, AnyObject>
+            
+            let results:Dictionary<String, AnyObject>! = json
+            
+            
+            DispatchQueue.main.async(execute: {
+                
+                /*if let weather = results["weather"] as? [[String:Any]], !weather.isEmpty {
+                    //print(weather[0]["description"]!) // the value is an optional.
+                    
+                    self.weatherCode = NumberFormatter().number(from: "\(weather[0]["id"]!)") as Int!
+                    //print(weather[0])
+                    self.weatherText = "\(weather[0]["description"]!)"
+                    
+                    let weatherIcon = "\(weather[0]["icon"]!)"
+                    
+                    self.imgIconUrl  = URL(string: "http://openweathermap.org/img/w/\(weatherIcon).png")
+                }*/
+                
+                //self.tableView.reloadData()
+                let kelvin = results["main"]?["temp"] as! Double
+                let celsius = round(kelvin - 273.15)
+                
+                let hPa = results["main"]?["pressure"] as! Double
+                
+                let pressure = round(hPa / 1013.25)
+                let doubleStr = String(format: "%.2f", pressure) // "3.14"
+                self.destinationWeatherView.temperatureLabel.text = "Temperature: \(NSNumber(value: celsius))ºC"
+                self.destinationWeatherView.humidityLabel.text = "Humidity: \(NSNumber(value: (results["main"]?["humidity"]) as! Double))%"
+                self.destinationWeatherView.pressureLabel.text = "Pressure: \(hPa) hPa"
             })
             
             }.resume()
